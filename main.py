75ed1e8cb051a5c4bd7577abc804d196e691f6dd
_ziyiyap <yapziyi13@gmail.com>
2026-08-10 23:10:19 +0800

    more data and selections

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import subprocess
 import pandas as pd
-import time
 from InquirerPy import inquirer
 interface = f"""
 {"-" * 20}
@@ -15,30 +14,75 @@ interface = f"""
             """
 
 def select_city():
-    data = pd.read_csv("worldcities.csv")
-    countries = data["country"].drop_duplicates()
+    city_data = pd.read_csv("malaysia_hotels.csv")
     subprocess.run("cls", shell=True)
-    country = inquirer.fuzzy(
-        message="--- Countries ---",
-        choices =  countries.sort_values().tolist(),
-        height = 10
-    ).execute()
 
-    cities = inquirer.fuzzy(
-        message= f"--- Cities in {country} ---",
-        choices = data[data["country"] == country]["city"],
+    city = inquirer.fuzzy(
+        message= f"--- Cities ---",
+        choices = city_data[" cityName"].drop_duplicates(),
         height= 10
     ).execute()
 
-    print("You selected %s" %cities)
+    return city
     
 
 
 def reserve_car():
-    select_city()
+    car_data = pd.read_csv("car_models.csv")
+    subprocess.run("cls", shell=True)
+    choice = inquirer.select(
+        message=f"{"-" * 20}\nCars\n{"-" * 20}",
+        choices = ["Full Criteria Search", "Based on Segment"]
+    ).execute()
+
+    city = select_city()
+
+    if choice == "Full Criteria Search":
+        # Full criteria includes brand, segment (specific)
+
+        brand = inquirer.fuzzy(
+            message= f"--- Car Brands ---",
+            choices = car_data["Maker"].drop_duplicates(),
+            height= 10
+        ).execute()
+
+        brand_segment = inquirer.fuzzy(
+            message = f"--- Segments for {brand} ---",
+            choices = car_data[car_data["Maker"] == brand]["Segment"].drop_duplicates(),
+            height= 10
+        ).execute()
+
+        car_model = inquirer.fuzzy(
+            message= f"--- Models for {brand} {brand_segment} ---",
+            choices= car_data[(car_data["Maker"] == brand) & (car_data["Segment"] == brand_segment)]["Genmodel"].drop_duplicates(),
+            height= 10
+        ).execute()
+
+
+    elif choice == "Based on Segment":
+        #Includes segments, ignore brand
+        segment = inquirer.fuzzy( 
+            message= "--- Select Segment ---",
+            choices = car_data["Segment"].drop_duplicates(),
+            height= 10
+        ).execute()
+
+        segment_data = car_data[car_data["Segment"] == segment].drop_duplicates().reset_index(drop=True)
+
+        choices = [
+            f"{i+1}. " + " | ".join(str(value) for value in row)
+            for i, row in segment_data.iterrows()
+        ]
+        car_series = inquirer.select( #Series object
+            message= "-" * 20,
+            choices= choices,
+            height= 10
+        ).execute()
+
 
 def menu_interface():
     menu = True
+
     redirect = {
         "1" : None,
         "2" : None,
@@ -56,8 +100,6 @@ def menu_interface():
             menu = False
             break
         elif user_input not in redirect.keys():
-            print("Invalid choice")
-            time.sleep(1)
             continue
         
         return redirect[user_input]()
