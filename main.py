df3504240433af968aafb30318f04f8c2e7830c5
zyyappp <yapziyi13@1utar.my>
2026-08-13 09:55:13 +0800

    car_reservation json

--- main.py
+++ main.py
@@ -5,70 +5,7 @@ from selection_helper import select, search
 from login_helper import login_user
 from config import DATA_FOLDER, car_data, city_data
 from user_class import User
-from reservation import switch_city
-
-interface = f"""
-{"-" * 20}
-1. Hotels
-2. Flights
-3. Trains
-4. Cars
-5. Attractions
-6. Payment
-7. Quit
-{"-" * 20}
-            """
-    
-
-
-def reserve_car():
-    subprocess.run("cls", shell=True)
-
-    choice = select(f"{"-" * 20}\nCars\n{"-" * 20}", ["Full Criteria Search", "Based on Segment"])
-
-    if choice == "Full Criteria Search":
-        # Full criteria includes brand, segment (specific)
-
-
-        brand = search("--- Car Brands ---", car_data["Maker"].drop_duplicates())
-        segment = search(f"--- Segments for {brand} ---", car_data[car_data["Maker"] == brand]["Segment"].drop_duplicates())
-
-        car_model = search(f"--- Models for {brand} {segment} ---", car_data[(car_data["Maker"] == brand) & (car_data["Segment"] == segment)]["Genmodel"].drop_duplicates())
-
-        car_price = float(car_data[(car_data["Maker"] == brand) & (car_data["Segment"] == segment) & (car_data["Genmodel"] == car_model)]["Rental_price"].to_list()[0])
-
-
-    elif choice == "Based on Segment":
-        #Includes segments, ignore brand
-        segment = search("--- Select Segment ---", car_data["Segment"].drop_duplicates() )
-
-
-        segment_data = car_data[car_data["Segment"] == segment].drop_duplicates().reset_index(drop=True)
-
-        choices = [
-            f"{i+1}. " + " | ".join(str(value) for value in row)
-            for i, row in segment_data.iterrows()
-        ]
-        selected = select("--- Select Car ---", choices)
-
-        selected_index = choices.index(selected)
-
-        brand = segment_data.iloc[selected_index]["Maker"]
-        car_model = segment_data.iloc[selected_index]["Genmodel"]
-        car_price = float(segment_data.iloc[selected_index]["Rental_price"])
-
-    car_details = f"""
-{"-" * 20}
-Brand : {brand}
-Model : {car_model}
-Segment : {segment}
-Price : RM {car_price}
-{"-" * 20}
-"""
-    print(car_details)
-
-    option = select("Enter option", ["Proceed to checkout", "Continue with another reservation", "Quit"])
-
+from reservation import switch_city, reserve_car
 
 def menu_interface():
     user = login_user()
@@ -96,9 +33,10 @@ def menu_interface():
         elif user_input not in redirect.keys():
             continue
         elif user_input == "Switch City":
-            return switch_city(user.id)
-        
-        return redirect[user_input]()
+            user.city = switch_city(user.id)
+            continue
+
+        redirect[user_input](user.id)
         
 
 menu_interface()
--- reservation.py
+++ reservation.py
@@ -1,7 +1,7 @@
 import subprocess
-from config import DATA_FOLDER, RESERVES_PATH, city_data
+from config import DATA_FOLDER, RESERVES_PATH, city_data, car_data
 import pandas as pd
-from selection_helper import search
+from selection_helper import search, select
 import json
 
 def load_reserve():
@@ -39,4 +39,72 @@ def switch_city(id):
 
             dump_reserve(reserve_data)
             return city
-    select_city(id)
+    return select_city(id)
+
+
+def reserve_car(id):
+    subprocess.run("cls", shell=True)
+
+    choice = select(f"{"-" * 20}\nCars\n{"-" * 20}", ["Full Criteria Search", "Based on Segment"])
+
+    if choice == "Full Criteria Search":
+        # Full criteria includes brand, segment (specific)
+
+
+        brand = search("--- Car Brands ---", car_data["Maker"].drop_duplicates())
+        segment = search(f"--- Segments for {brand} ---", car_data[car_data["Maker"] == brand]["Segment"].drop_duplicates())
+
+        car_model = search(f"--- Models for {brand} {segment} ---", car_data[(car_data["Maker"] == brand) & (car_data["Segment"] == segment)]["Genmodel"].drop_duplicates())
+
+        car_price = float(car_data[(car_data["Maker"] == brand) & (car_data["Segment"] == segment) & (car_data["Genmodel"] == car_model)]["Rental_price"].to_list()[0])
+
+
+    elif choice == "Based on Segment":
+        #Includes segments, ignore brand
+        segment = search("--- Select Segment ---", car_data["Segment"].drop_duplicates() )
+
+
+        segment_data = car_data[car_data["Segment"] == segment].drop_duplicates().reset_index(drop=True)
+
+        choices = [
+            f"{i+1}. " + " | ".join(str(value) for value in row)
+            for i, row in segment_data.iterrows()
+        ]
+        selected = select("--- Select Car ---", choices)
+
+        selected_index = choices.index(selected)
+
+        brand = segment_data.iloc[selected_index]["Maker"]
+        car_model = segment_data.iloc[selected_index]["Genmodel"]
+        car_price = float(segment_data.iloc[selected_index]["Rental_price"])
+
+    car_details = f"""
+{"-" * 20}
+Brand : {brand}
+Model : {car_model}
+Segment : {segment}
+Price : RM {car_price}
+{"-" * 20}
+"""
+    print(car_details)
+
+    reserve_data = load_reserve()
+
+    for data in reserve_data:
+        if data["id"] == id:
+            if data.get("car") is None:
+                data["car"] = []
+                
+            data["car"].append({
+                "brand" : brand,
+                "model" : car_model,
+                "segment" : segment,
+                "price" : car_price
+            })
+
+    dump_reserve(reserve_data)
+
+    option = select("Enter option", ["Proceed to checkout", "Continue with another reservation", "Quit"])
+
+    if option == "Continue with another reservation":
+        return
