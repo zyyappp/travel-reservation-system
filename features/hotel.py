8e40e795332ee5459b8701daa99d215c8afd689e
_ziyiyap <yapziyi13@gmail.com>
2026-08-19 22:39:09 +0800

    Filter function for hotel

--- features/hotel.py
+++ features/hotel.py
@@ -9,6 +9,8 @@ from datetime import datetime
 import math
 from helpers.availability_helper import initialize_availability, dump_availability
 
+#backlog: make reserve_hotel() more clean by splitting the function into more smaller functions
+
 def rating(HotelRating):
 
     ratings = ["one", "two", "three", "four", "five"]
@@ -17,48 +19,106 @@ def rating(HotelRating):
     return "★" * (ratings.index(HotelRating.lower()[:-4]) + 1) if HotelRating.lower()[:-4] in ratings else "N/A"
 
 def reserve_hotel(user_id):
+    #filtering
     clear()
 
-    current_page = "hotel_selection"
+    current_page = "hotel_search"
     user_data = load_reserve()
     availability = initialize_availability()
+
 
     account = next(
         data for data in user_data if data["id"] == user_id
     )
-
-    specific_hotel_data = hotel_data[hotel_data["cityName"] == account["city"]].reset_index(drop=True)
 
     if account.get("hotel") is None:
         account["hotel"] = []
 
 
     heading = f"""
-    {'─' * 60}
-                           HOTELS IN {account["city"]}
-    {'─' * 60}
+{'─' * 60}
+HOTELS IN {account["city"].upper()}
+{'─' * 60}
     """
 
-
-
-    choices = [
-        (f"{i+1}. {data["HotelName"]} | {rating(data["HotelRating"])} | RM {data["price"]:.2f}")
-        for i, data in specific_hotel_data.iterrows()
-    ]  
+    user_hotel = hotel_data[hotel_data["cityName"] == account["city"]].reset_index(drop=True)
+    user_filter = "Default"
 
     while current_page != "finished":
 
+        if current_page == "hotel_search":
+            clear()
+            search_heading = f"""
+{'─' * 60}
+HOTEL SEARCH
+{'─' * 60}
+
+Filter: {user_filter}
+"""     
+            print(search_heading)
+            browse_selection = ["Browse hotels", "Change filters", "Reset filters"]
+            browse_or_sort = select("", browse_selection)
+
+            if browse_or_sort == "BACK":
+                current_page = "finished"
+                return
+            elif browse_or_sort == browse_selection[0]:
+                current_page = "hotel_selection"
+            elif browse_or_sort == browse_selection[1]:
+                current_page = "change_filters"
+            elif browse_or_sort == browse_selection[2]:
+                user_hotel = hotel_data[hotel_data["cityName"] == account["city"]].reset_index(drop=True)
+                user_filter = "Default"
+
+        if current_page == "change_filters":
+            clear()
+
+            filter_choices = ["Default", "Price: Low → High", "Price: High → Low", "Rating: Low → High", "Rating: High → Low", "Name: A → Z"]
+            user_filter = select("Sort by:", filter_choices)
+
+            if user_filter == "BACK":
+                current_page = "hotel_search"
+                continue
+            elif user_filter == filter_choices[0]:
+                user_hotel = hotel_data[hotel_data["cityName"] == account["city"]].reset_index(drop=True)
+            elif user_filter == filter_choices[1]:
+                user_hotel = user_hotel.sort_values("price").reset_index(drop=True)
+            elif user_filter == filter_choices[2]:
+                user_hotel = user_hotel.sort_values("price", ascending = False).reset_index(drop=True)
+            elif user_filter == filter_choices[3]:
+                user_hotel = user_hotel.sort_values(
+                    by = "HotelRating",
+                    key = lambda series: series.map(lambda r : len(rating(r)) if rating(r) != "N/A" else 0), # series = whole HotelRating series -> (each value mapped to r) -> passed to function
+                ).reset_index(drop = True)
+            elif user_filter == filter_choices[4]:
+                user_hotel = user_hotel.sort_values(
+                    by = "HotelRating",
+                    key = lambda series: series.map(lambda r : len(rating(r)) if rating(r) != "N/A" else 0) ,
+                    ascending = False
+                ).reset_index(drop = True)
+            elif user_filter == filter_choices[5]:
+                user_hotel = user_hotel.sort_values("HotelName").reset_index(drop = True)
+
+            current_page = "hotel_search"
+
+            
         if current_page == "hotel_selection":
+            choices = [
+        (f"{i+1}. {data["HotelName"]} | {rating(data["HotelRating"])} | RM {data["price"]:.2f}")
+        for i, data in user_hotel.iterrows()
+                        ]  
+            clear()
             print(heading)
+
             hotel = search("", choices, 10)
 
             if hotel == "BACK":
-                current_page = "finished"
-                return
+                current_page = "hotel_search"
+
             elif hotel is not None:
                 choice_index = choices.index(hotel)
 
-                hotel_details = specific_hotel_data.iloc[choice_index]
+                hotel_details = user_hotel.iloc[choice_index]
                 hotel_name = hotel_details["HotelName"]
                 hotel_rating = rating(hotel_details["HotelRating"])
                 hotel_address = hotel_details["Address"]
@@ -89,7 +149,7 @@ def reserve_hotel(user_id):
 {hotel_desc[0][:-1]}
 
 Facilities:
-{" • ".join(hotel_facilities[:4])}
+{" • ".join(facility.capitalize() for facility in hotel_facilities[:4])}
 
 From RM {hotel_price:.2f} per night
 
@@ -118,7 +178,7 @@ ABOUT
 {"\n".join(hotel_desc[:2])}
 
 FACILITIES
-{"• " + "\n• ".join(hotel_facilities[:10])}
+{"• " + "\n• ".join(facility.capitalize() for facility in hotel_facilities[:10])}
 
 CONTACT
 Fax: {hotel_fax}
@@ -241,7 +301,7 @@ Base price: RM {hotel_price:.2f} / night
 
     if option == options[0]:
         return payment(user_id)
-    elif option == options[1]:
+    elif option == options[1] or option == "BACK":
         return
     elif option == options[2]:
         quit()
